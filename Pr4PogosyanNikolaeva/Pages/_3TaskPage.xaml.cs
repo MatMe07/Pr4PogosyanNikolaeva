6cefd818992d2bd95424dacb5fe6736fdfc7a8e6
MatMe07 <memat0403@gmail.com>
2026-02-24 09:53:32 +0300

    Созданы все странички

---
+++ Pr4PogosyanNikolaeva/Pages/_3TaskPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pr4PogosyanNikolaeva.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для _3TaskPage.xaml
+    /// </summary>
+    public partial class _3TaskPage : Page
+    {
+        public _3TaskPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
