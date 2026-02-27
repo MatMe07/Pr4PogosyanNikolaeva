4de5d38a63ef1afa9006694f9ca6dcb7816ab267
kiokokun200-gif <kiokokun200@gmail.com>
2026-02-27 16:06:15 +0300

    сделана первая страница

--- Pr4PogosyanNikolaeva/Pages/_1TaskPage.xaml.cs
+++ Pr4PogosyanNikolaeva/Pages/_1TaskPage.xaml.cs
@@ -20,9 +20,66 @@ namespace Pr4PogosyanNikolaeva.Pages
     /// </summary>
     public partial class _1TaskPage : Page
     {
+        public double x = 0, y = 0, z = 0, res = 0;
+        public bool x_znak = false, y_znak = false, z_znak = false;
+
         public _1TaskPage()
         {
             InitializeComponent();
+        }
+
+        private void BtnCalculate_Click(object sender, RoutedEventArgs e)
+        {
+          
+
+            res = (Math.Pow(
+               y + Math.Pow(x - 1, (1.0 / 3.0)), (1.0 / 4.0)))
+                /
+                (Math.Abs(x - y) * (
+                Math.Pow(Math.Sin(z), 2)
+                + Math.Tan(z)));
+            TxtBoxRes.Text = res.ToString();
+         
+
+        }
+
+        private void TxtBoxZ_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            CheckInput.CheckNullOrWSpace(TxtBoxZ);
+        }
+
+        private void TxtBoxY_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            CheckInput.CheckNullOrWSpace(TxtBoxY);
+        }
+
+        private void TxtBoxX_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            CheckInput.CheckNullOrWSpace(TxtBoxX);
+        }
+
+        private void BtnClear_Click(object sender, RoutedEventArgs e)
+        {
+            TxtBoxX.Text = "0";
+            TxtBoxY.Text = "0";
+            TxtBoxZ.Text = "0";
+            TxtBoxRes.Text = "0";
+            
+        }
+
+        private void TxtBoxX_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !CheckInput.Check(e.Text[0], TxtBoxX.Text + e.Text[0], out x, ref x_znak);
+        }
+
+        private void TxtBoxY_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !CheckInput.Check(e.Text[0], TxtBoxY.Text + e.Text[0], out y, ref y_znak);
+        }
+
+        private void TxtBoxZ_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !CheckInput.Check(e.Text[0], TxtBoxZ.Text + e.Text[0], out z, ref z_znak);
         }
     }
 }
