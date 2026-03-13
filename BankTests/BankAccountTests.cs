558121a7c92d4890d2b85ca1f1f6f8a97fa45cf7
kiokokun200-gif <kiokokun200@gmail.com>
2026-03-13 13:34:48 +0300

    #

---
+++ BankTests/BankAccountTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using BankAccountNS;
+
+namespace BankTests
+{
+    [TestClass]
+    public class BankAccountTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
