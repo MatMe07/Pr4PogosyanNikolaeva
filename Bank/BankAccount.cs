c162775a09f1ce6ffffc61ec55a81d87ac5a2bd4
MatMe07 <memat0403@gmail.com>
2026-03-13 14:55:35 +0300

    Рефакторинг Credit

--- Bank/BankAccount.cs
+++ Bank/BankAccount.cs
@@ -11,6 +11,7 @@ namespace BankAccountNS
         private double m_balance;
         public const string DebitAmountExceedsBalanceMessage = "Debit amount exceeds balance";
         public const string DebitAmountLessThanZeroMessage = "Debit amount is less than zero";
+        public const string CreditAmountLessThanZeroMessage = "Credit amount is less than zero";
 
         private BankAccount() { }
 
@@ -61,7 +62,8 @@ namespace BankAccountNS
         {
             if (amount < 0)
             {
-                throw new ArgumentOutOfRangeException("amount");
+                throw new System.ArgumentOutOfRangeException("amount", amount, CreditAmountLessThanZeroMessage);
+
             }
 
             m_balance += amount;
