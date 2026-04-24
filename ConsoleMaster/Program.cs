51e1a86040f96881d29e1079a88acf3314322c1f
MatMe07 <memat0403@gmail.com>
2026-04-24 14:22:14 +0300

    #

--- ConsoleMaster/Program.cs
+++ ConsoleMaster/Program.cs
@@ -10,8 +10,30 @@ namespace ConsoleMaster
     {
         static void Main(string[] args)
         {
+            int result = Fibonacci(6);
+            Console.WriteLine(result);
+        }
 
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        static int Fibonacci(int n)
+        {
+            Console.WriteLine("The output is: ");
+            int n1 = 0;
+            int n2 = 1;
+            int sum;
+        
+            for (int i = 2; i <= n; i++)
+            {
+                sum = n1 + n2;
+                n1 = n2;
+                n2 = sum;
+            }
+        
+            return n == 0 ? n1 : n2;
         }
     }
 }
